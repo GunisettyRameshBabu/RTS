32a0c4d403d53acaeabb094c1ff826bc64600ad3
Ramesh Babu Gunisetty <ramesh.babu.gunisetty@volvo.com>
2020-03-06 01:01:07 +0530

    SC Changes

---
+++ SCTimeSheet/Models/EmployeeProjectListUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SCTimeSheet.Models
+{
+    public class EmployeeProjectListUI
+    {
+        [Key]
+        public Int64 EmployeeID { get; set; }
+
+        [Display(Name = "Project Member")]
+        public string EmpName { get; set; }
+
+        [UIHint("IsManager")]
+        [Display(Name = "Project Manager")]
+        public string IsManager { get; set; }
+
+        [UIHint("ProjectMemberInvPercentage")]
+        [Display(Name = "% of Project Involvement")]
+        public decimal InvPercentage { get; set; }
+
+        [UIHint("ProjectMemberStartDate")]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [UIHint("ProjectMemberEndDate")]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        
+        [Display(Name = "Roles")]
+        public Int64 RoleID { get; set; }
+
+        public int IsRDProject { get; set; }
+
+        [UIHint("ProjectMemberRole")]
+        [Display(Name ="Roles")]
+        public long? EmpRole { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public DateTime? JoinDate { get; set; }
+    }
+}
